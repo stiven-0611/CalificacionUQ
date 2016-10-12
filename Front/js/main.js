6cc3ad9debf5ba1d6214d7056041ecf8fa3b4b42
stiven-0611 <stiven-0611@hotmail.com>
2016-10-12 15:41:02 -0500

    Menu lateral

---
+++ Front/js/main.js
@@ -0,0 +1,9 @@
+$(".dropdown-button").dropdown();
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
+
+  
