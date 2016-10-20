ee8ab613fe46421d5f8174cb81a86c4b195e3d0c
luisa-sw3 <luisita231@gmail.com>
2016-10-20 15:15:46 -0500

    Nueva Actividad
    Se borran clases que impiden el correcto funcionamiento del la
    plataforma y se implementa la funcionalidad de agregar una nueva
    actividad. Queda pendiente el jquery que agrega la actividad al final de
    la lista

--- Front/js/docente.js
+++ Front/js/docente.js
@@ -7,3 +7,50 @@ function cargarVista(nombreVista) {
 }
 
 cargarVista("misCursos");
+
+$(document).ready(function(){
+
+	$('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	 });
+
+	 $('#description').trigger('autoresize');
+
+	 $('#formulario').validate ({
+	 	rules:{
+	 		activity: "required",
+	 		date: "required"
+	 	},
+
+	 	messages:{
+	 		activity: "Asignele un nombre a la actividad",
+	 		date: "Seleccione una fecha para la entrega"
+	 	},
+
+	 	submitHandler: function() {
+	 		var activity = $("#actividad").val();
+	 		var fecha = $("#date").val();
+      var now = $.now();
+      var date = new Date(now);
+
+      var elemNuevo = $('<tr><td class="nameActivity"> </td> <td class="dateActivity"> </td> </tr>');
+
+      elemNuevo.find(".nameActivity").text(activity);
+
+      elemNuevo.find(".dateActivity").text(fecha);
+
+      elemNuevo.appendTo("#tablaActividades");
+
+	 	}
+	 });
+
+});
+
+function cancel (){
+	var url = "../viewsDocente/listaActividades.html";
+
+	$.get(url, function(respuestaServer) {
+		$("#contenedor").html(respuestaServer);
+	});
+}
