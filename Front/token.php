bdcd6ab7add79dee6ac6ef83cd5e7cc85bbb8469
stiven-0611 <stiven-0611@hotmail.com>
2016-10-20 17:26:49 -0500

    Modificacion log

--- Front/token.php
+++ Front/token.php
@@ -3,21 +3,13 @@
 $data = 'client_id=' . '46779082d184c98cdcdb' . '&' .
 		'client_secret=' . '9256d52088a1631865ea4327b0c5fc1c4fcf6bd1' . '&' .
 		'code=' . urlencode($_GET['code']);
-		$url ='https://github.com/login/oauth/access_token?'.$data;
+		$url =''.$data;
 
-print_r($data);
-
+print_r($url);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-
-preg_match('/access_token=([0-9a-f]+)/', $response, $out);
-echo $out[1];
+print_r($response);
 curl_close($ch);
-//$ch = curl_init($url);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//$response = curl_exec($ch);
-//print_r($response);
-//curl_close($ch);
 
 ?>
