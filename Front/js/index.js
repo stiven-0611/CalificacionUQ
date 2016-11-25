320d242c3bd94528b61e4d996211fa7563bba69a
stiven-0611 <stiven-0611@hotmail.com>
2016-11-25 16:08:15 -0500

    Cambio index

---
+++ Front/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+        // Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
